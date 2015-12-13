e17286ac393fc66a2dd0a32d1703b5a4ee011be7
Schatzman <devdan1984@gmail.com>
2015-12-13 00:24:54 -0700

    trying to authenticate with passport

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ var http = require('http'),
     methodOverride = require('method-override'),
     session = require('express-session'),
     passport = require('passport'),
-    LocalStrategy = require('passport-local'),
+    LocalStrategy = require('passport-local').Strategy,
     TwitterStrategy = require('passport-twitter'),
     GoogleStrategy = require('passport-google'),
     FacebookStrategy = require('passport-facebook'),
-    app = express(),
-    MongoClient = require('mongodb').MongoClient,
-    ObjectId = require('mongodb').ObjectId,
-    db;
+    app = express();
 
 //===================PASSPORT====================
 
@@ -32,22 +29,24 @@ saveUninitialized: true, resave: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+console.log(LocalStrategy);
 // Session-persisted message middleware
-app.use(function(req, res, next){
-    var err = req.session.error,
-        msg = req.session.notice,
-        success = req.session.success;
+app.use(
+    function(req, res, next){
+        var err = req.session.error,
+            msg = req.session.notice,
+            success = req.session.success;
 
-    delete req.session.error;
-    delete req.session.success;
-    delete req.session.notice;
+        delete req.session.error;
+        delete req.session.success;
+        delete req.session.notice;
 
-    if (err) res.locals.error = err;
-    if (msg) res.locals.notice = msg;
-    if (success) res.locals.success = success;
+        if (err) res.locals.error = err;
+        if (msg) res.locals.notice = msg;
+        if (success) res.locals.success = success;
 
-    next();
-});
+        next();
+    });
 
 // Configure express to use handlebars templates
 var hbs = exphbs.create({
@@ -68,25 +67,49 @@ var port = process.env.PORT || 5000,
     console.log('MEAN RPG app listening at http://localhost:%s', port);
 });
 
-app.get('/', function (req, res) {
+app.get('/index.html', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.use('/static', express.static(__dirname + '/bower_components'));
 app.use('/static/public', express.static(__dirname + '/public'));
 
-
-//====================DATABASE===================
-
-//
-
-//====================CONNECTION=================
-MongoClient.connect('mongodb://localhost:27017/rpg/', function (err, database) {
-    if (!err) {
-        console.log('Connected to database rpg.');
-        db = database;
-    } else {
-        throw err;
-    }
+//===============ROUTES=================
+//displays our homepage
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
 });
+
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin',
+  failureFlash: true
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+var config = require('./config.js'), //config file contains all tokens and other private info
+    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
+console.log(LocalStrategy);
 
 
